9c3e3e9103d04fa1b164889d3910bc7bbd0b2bb1
miguelzackk <mzsilvaw@gmail.com>
2026-08-25 10:59:00 -0300

    feat: add registration screen and update app structure

--- ShopSwift/src/App.js
+++ ShopSwift/src/App.js
@@ -1,13 +1,9 @@
-import { Button, Image, Pressable, Text, TextInput, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Illustration from '../assets/android-icon-foreground.png'
-import { Card } from './components/Card';
-import { Login } from './screens/Login';
+import {SafeAreaView} from 'react-native-safe-area-context'
+import {Register} from "./screens/Cadastro";
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <Login/>     
-    </SafeAreaView>
-  );
+    return (<SafeAreaView>
+        {/*<Login/>*/}
+        <Register/>
+    </SafeAreaView>);
 }
---
+++ ShopSwift/src/screens/Cadastro/index.js
@@ -0,0 +1,39 @@
+import {Image, Pressable, Text, TextInput, View} from 'react-native'
+import {styles} from './style'
+import logo from '../../assets/logoShopSwift.png'
+
+export const Register = () => {
+    return (
+
+        <View style={styles.containerHome}>
+            <Image source={logo}/>
+            <View style={styles.contentHome}>
+                <Text style={styles.txtTitulo}>Crie uma conta <Text
+                    style={styles.txtHighlights}>e descubra...</Text></Text>
+                <View style={styles.containerForm}>
+                    <View style={styles.containerInputs}>
+                        <View>
+                            <Text>E-mail:</Text>
+                            <TextInput keyboardType='email-address' style={styles.input}/>
+                        </View>
+                        <View>
+                            <Text>Senha:</Text>
+                            <TextInput secureTextEntry={true} style={styles.input}/>
+                        </View>
+                        <View>
+                            <Text>Confirmar Senha:</Text>
+                            <TextInput secureTextEntry={true} style={styles.input}/>
+                        </View>
+                    </View>
+                    <Pressable style={styles.buttonLogin}>
+                        <Text style={styles.txtLogin}>Cadastrar</Text>
+                    </Pressable>
+                </View>
+
+                <Text style={styles.txtNaoTemConta}>Tem uma conta? <Text
+                    style={styles.txtCriarConta}>Logar</Text></Text>
+            </View>
+        </View>
+
+    )
+}
